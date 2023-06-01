3d815f14491dc88677f171231b2afdd894419566
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2023-06-01 18:23:43 +0200

    fixed Season Summary

--- js/season-results.js
+++ js/season-results.js
@@ -61,6 +61,45 @@ function populateCumulativeResultsTable(sailboatDTOs, cumulativeResultsTable) {
     });
 }
 
+async function fetchSailboatCount() {
+    const response = await fetch('http://localhost:8080/api/sailboats/count');
+    return await response.json();
+}
+
+async function fetchRaceCount() {
+    const response = await fetch('http://localhost:8080/api/races/count');
+    return await response.json();
+}
+
+function createStatRow(label, value) {
+    const row = document.createElement('tr');
+    const labelCell = document.createElement('td');
+    const valueCell = document.createElement('td');
+
+    labelCell.textContent = label;
+    valueCell.textContent = value;
+
+    row.appendChild(labelCell);
+    row.appendChild(valueCell);
+
+    return row;
+}
+
+async function populateSummaryStats() {
+    const sailboatCount = await fetchSailboatCount();
+    const raceCount = await fetchRaceCount();
+
+    const summaryStatsTableBody = document.querySelector('table[summaryStats] tbody');
+
+    // Append stats
+    summaryStatsTableBody.appendChild(createStatRow('Total Sailboats', sailboatCount));
+    summaryStatsTableBody.appendChild(createStatRow('Total Races', raceCount));
+}
+
+
+
+
+
 async function populateTables() {
     const raceDTOs = await fetchRacesDTO();
     const sailboatDTOs = await fetchSailboatCumulativeResultsDTO();
@@ -72,4 +111,6 @@ async function populateTables() {
     populateCumulativeResultsTable(sailboatDTOs, cumulativeResultsTable);
 }
 
+populateSummaryStats();
 populateTables();
+
